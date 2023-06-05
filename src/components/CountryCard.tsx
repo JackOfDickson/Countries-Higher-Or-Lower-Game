25090e92350da1dc8d8bcc2ee8582b538d2e5f3f
Jack Dickson <jackowendickson@gmail.com>
2023-06-05 13:44:02 +0100

    changed how flag is displayed and changed the styled component for the CountryCard comp

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -7,13 +7,26 @@ type CountryCardProps = {
     guess?: Function
 }
 
-const CountryCardDiv= styled.div<{country: Country}> `
-    background-image:url(${(props) => props.country.flags.png});
+const CountryCardDiv= styled.div<{country: Country}>`
+    /* background-image:url(${(props) => props.country.flags.png});
     background-size: auto;
     height: 200px;
     width: 300px;
     background-repeat: no-repeat;
-    background-position: center;
+    background-position: center; */
+    background-color: #5492ff;
+    border: 2px solid black;
+    border-radius: 30px;
+    &>p {
+        /* color: red; */
+        
+    }
+    &>img{
+        border-top: 2px solid black;
+        border-bottom: 2px solid black;
+        height: 200px;
+        width: 300px;
+    }
     `
 
 const CountryOption = ({country, showPopulation, guess} : CountryCardProps) => {
@@ -29,9 +42,10 @@ const CountryOption = ({country, showPopulation, guess} : CountryCardProps) => {
     return (
         <CountryCardDiv country={country}>
             <p>{country?.name.common}</p>
+            <img src={country.flags.png}/> 
 
             {showPopulation? 
-            <p> {country?.population.toLocaleString()} </p> 
+            <p> {country?.population.toLocaleString()} </p>
             : //or 
             <div>
                 <button onClick={handleClickHigher}> Higher </button>
